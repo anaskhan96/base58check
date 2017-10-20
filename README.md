953daf4a26e3a120bfcac7791153212827596c05
Anas Khan <anas.khan96@outlook.com>
2017-10-20 20:43:06 +0530

    added test file

--- README.md
+++ README.md
@@ -42,6 +42,6 @@ func main() {
 
 ```
 
-### Reference
+### References
 
 + [Base58Check encoding](https://en.bitcoin.it/wiki/Base58Check_encoding)
---
+++ base58check_test.go
@@ -0,0 +1,29 @@
+package base58check
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+	encoded, err := Encode("00", "44D00F6EB2E5491CD7AB7E7185D81B67A23C4980F62B2ED0914D32B7EB1C5581")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	actual := "1XJjHG4gLiJfxrx82yPFWC8tu8cxKvaQjZNvVfSrsfiX4mbUsw"
+	if !reflect.DeepEqual(encoded, actual) {
+		t.Errorf("Expected %s, got %s", actual, encoded)
+	}
+}
+
+func TestDecode(t *testing.T) {
+	decoded, err := Decode("5JLbJxi9koHHvyFEAERHLYwG7VxYATnf8YdA9fiC6kXMghkYXpk")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	actual := strings.ToLower("44D00F6EB2E5491CD7AB7E7185D81B67A23C4980F62B2ED0914D32B7EB1C5581")
+	if !reflect.DeepEqual(decoded, actual) {
+		t.Errorf("Expected %s, got %s", actual, decoded)
+	}
+}
